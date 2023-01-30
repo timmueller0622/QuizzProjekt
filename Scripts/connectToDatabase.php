462df59796a8fa613823cca1adc5694c63985504
timmueller0622 <Tim.Mueller@edu.bib.de>
2023-01-30 11:42:27 +0100

    testing pdo 3

--- Scripts/connectToDatabase.php
+++ Scripts/connectToDatabase.php
@@ -2,9 +2,9 @@
     //connect.php
     echo "\ntestconnectenter";
     try {
-        $pdo = new PDO('oracle:host=quizzteam2.jedimasters.net;dbname=Team2','opc','QuizzApp9755');
+        $pdo = new PDO('oracle:dbname=Team2;host=quizzteam2.jedimasters.net','opc','QuizzApp9755');
     } catch (Exception $e){
-        echo ($e);
+        echo ($e->getMessage());
     }
     echo "\ntestconnectreturn";
 ?>
