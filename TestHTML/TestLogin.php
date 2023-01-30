8e0e5154ad9ddc14ef207a45711d68d1a79595f0
Julius Szumski <Julius.Szumski@edu.bib.de>
2023-01-30 09:10:30 +0100

    TestHtml Sites Added

---
+++ TestHTML/TestLogin.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>LogIn</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>
+                <b>Login</b>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <b>Username</b>
+            </td>
+            <td>
+                <input type="text" id="Username">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <b>Password</b>
+            </td>
+            <td>
+                <input type="text" id="Password">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit">
+            </td>
+        </tr>
+    </table>
+    <
+</body>
+</html>
---
+++ TestHTML/TestRegistration.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>
+                <b>Registrations</b>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <b>Email</b>
+            </td>
+            <td>
+                <input type="email" id="email">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <b>Brithday</b>
+            </td>
+            <td>
+                <input type="date" id="Birthday">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <b>Username</b>
+            </td>
+            <td>
+                <input type="text" id="Username">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <b>Password</b>
+            </td>
+            <td>
+                <input type="password" id="Password">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit">
+            </td>
+        </tr>
+    </table>
+    <
+</body>
+</html>
