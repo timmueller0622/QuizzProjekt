e91e0c4d72ba97f46b31b94dcbb0024b77ef1ee5
Marc Pape <Marc.Pape@edu.bib.de>
2023-01-30 12:15:02 +0100

    changed location to go when no session started

--- Scripts/User/userLogin.php
+++ Scripts/User/userLogin.php
@@ -2,6 +2,6 @@
     session_start();
     require '../connectToDatabase.php';
     if (!isset($_SESSION["username"])){
-        header('Location: loginAPI.php');
+        header('Location: ../Admin/loginAPI.php');
     }
 ?>
