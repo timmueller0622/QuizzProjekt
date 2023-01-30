a692ac8b70e8327649581d5623d1ca2b4f7189a2
Marc Pape <Marc.Pape@edu.bib.de>
2023-01-30 09:29:19 +0100

    new Array with tempObjects for test

---
+++ tempObjectsForLogin.php
@@ -0,0 +1,10 @@
+<?php
+
+/*Temporäre Objekte, um funktionen zu testen.*/
+array(
+    1 => new Benutzer("Julius", "bib", "julius.szumski@bib.de", new DateTime('4.4.2002', new DateTimeZone("GMT+2"))),
+    2 => new Benutzer("Marc", "bib", "marc.pape@bib.de", new DateTime('22.2.2003', new DateTimeZone("GMT+2"))),
+    3 => new Benutzer("Tim", "bib", "tim.mueller@bib.de", new DateTime('1.1.1994', new DateTimeZone("GMT+2")))
+)
+
+?>
