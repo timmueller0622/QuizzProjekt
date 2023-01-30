fd5f556ad58a74b05c6bfda791d53fcdc84d969e
timmueller0622 <Tim.Mueller@edu.bib.de>
2023-01-30 12:40:36 +0100

    echo test

--- Scripts/connectToDatabase.php
+++ Scripts/connectToDatabase.php
@@ -2,6 +2,7 @@
     //connect.php
 
     try {
+        echo "test";
         $conn = new PDO("oci:dbname=quizzapp_high", "quizzteam2", "QuizzApp9755");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo 'Connected to database<br>';
