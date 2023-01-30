51572f61dc0615bbd014d3fde1ecbed6fb23da74
Marc Pape <Marc.Pape@edu.bib.de>
2023-01-30 14:29:24 +0100

    resetMailTest and Edit

--- Scripts/User/userReset.php
+++ Scripts/User/userReset.php
@@ -4,4 +4,15 @@
     if (!isset($_SESSION["username"])){
         header('Location: ../Admin/loginAPI.php');
     }
+
+    class ResetUser{
+
+        public static function sendResetEmail($emailinput) : void{
+            mail($emailinput, "Quizapp reset Mail", "ResetTestMail", "From: QuizzappDevelopment <Marc.Pape@edu.bib.de>");
+        }
+
+    }
+
+
+    ResetUser::sendResetEmail("marc.pape-info@web.de");
 ?>
