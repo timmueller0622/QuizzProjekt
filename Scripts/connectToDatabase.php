62ace1300b2329ed42e01fd811692e7380139f04
timmueller0622 <Tim.Mueller@edu.bib.de>
2023-01-30 12:44:22 +0100

    testing again

--- Scripts/connectToDatabase.php
+++ Scripts/connectToDatabase.php
@@ -1,10 +1,10 @@
 <?php
     //connect.php
-
+    $tns = "quizzapp_high";
+    $user = "quizzteam2";
+    $password = "QuizzApp9755";
     try {
-        $conn = new PDO("oci:dbname=quizzapp_high", "quizzteam2", "QuizzApp9755");
-        
-        echo "test";
+        $conn = new PDO("oci:dbname=".$tns, $user, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo 'Connected to database<br>';
 
