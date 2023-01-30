dd0d401154358ebf7cb4404a1b42181169762368
timmueller0622 <Tim.Mueller@edu.bib.de>
2023-01-30 11:36:42 +0100

    change pdo variable

--- Scripts/connectToDatabase.php
+++ Scripts/connectToDatabase.php
@@ -1,6 +1,6 @@
 <?php
     //connect.php
     echo "\ntestconnectenter";
-    $pdo = new PDO('oracle:host=http://quizzteam2.jedimasters.net;dbname=Team2','root','');
+    $pdo = new PDO('oracle:host=quizzteam2.jedimasters.net;dbname=Team2','opc','QuizzApp9755');
     echo "\ntestconnectreturn";
 ?>
