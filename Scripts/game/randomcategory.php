25f7b4a63bd8700ff84361181c6fe16bb2c3386d
MarcP <marc.pape-info@web.de>
2023-01-31 16:41:45 +0100

    t

--- Scripts/game/randomcategory.php
+++ Scripts/game/randomcategory.php
@@ -1,7 +1,7 @@
 <?php
 /*Call this Script for API Request*/
 require 'choosecategory/categoryscript.php';
-$out = Category::getRandomCategory();
+//$out = Category::getRandomCategory();
 
 echo 
 '<!DOCTYPE html>
