061f5de98564c341f6d3756aee4e6e4066d898c7
Tim Müller <Tim.Mueller@edu.bib.de>
2023-01-31 14:32:58 +0100

    testing playerhinzufügen

--- Scripts/Admin/playerHinzufuegen.php
+++ Scripts/Admin/playerHinzufuegen.php
@@ -5,7 +5,6 @@
 	if (isset ($_POST['sub'])){
 		require 'connectToDatabase.php';
 		$sql = "INSERT INTO player (";
-		$vals = array();
         foreach($conn->query("SELECT * FROM player") as $r){
             for ($i=0; $i < sizeof(array_keys($r)); $i++) {
                 if (is_numeric(array_keys($r)[$i]))
@@ -15,11 +14,14 @@
 					$sql .= ", ";
             }
 			$sql .= ") VALUES (";
-			for ($i=0; $i < sizeof(array_keys($r)); $i++) {
+			for ($i=2; $i < sizeof(array_keys($r)); $i++) {
                 if (is_numeric(array_keys($r)[$i]))
                     continue;
-				if ($i == 0)
+				if ($i == 0){
+					$pcount = $conn->query("select count(playerid) from player");
+					echo $pcount;
 					$sql .= "NULL, ";
+				}
 				else{
                 	$sql .= "\"" . $_POST[array_keys($r)[$i]] . "\"";
 					if ($i < sizeof(array_keys($r))-2)
@@ -45,18 +47,13 @@
 	<body>
 		<form method="post">
 		<?php
-			echo "test1<br>";
 			require 'navi.php';
 			require '../connectToDatabase.php';
-			echo "test2<br>";
             $s = "";
             $s .= "<table align =\"center\" border= \"1\" cellpadding=\"10\" cellspacing=\"0\">";
             $s .= "<thead><tr><th>Data</th><th>Wert</th></tr></tr></thead><tbody>";
-			echo "test3<br>";
             foreach($conn->query("SELECT * FROM player") as $r){
-				echo "test4<br>";
                 for ($i=2; $i < sizeof(array_keys($r)); $i++) {
-					echo "test5<br>";
                     if (is_numeric(array_keys($r)[$i]))
                         continue;
                     $s .= "<tr><td>" . array_keys($r)[$i] . "</td>";
@@ -65,7 +62,6 @@
                 $s .= "<td></td><td><input type=\"submit\" name=\"sub\" value=\"Speichern\"></td>";
 				break;
             }
-			echo "test6<br>";
             $s .= "</tbody></table>";
             echo $s;
 		?>
