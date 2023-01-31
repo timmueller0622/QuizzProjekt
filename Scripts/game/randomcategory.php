8cd2868fb36bcc3c55a4efe15ddf461daba789de
MarcP <marc.pape-info@web.de>
2023-01-31 16:22:40 +0100

    html surroundings in randomcategory.php

--- Scripts/game/randomcategory.php
+++ Scripts/game/randomcategory.php
@@ -1,5 +1,17 @@
 <?php
 
+echo 
+"<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>randomcategory</title>
+</head>
+<body>
+";
+
 /*Call this Script for API Request*/
 
     try{
@@ -10,7 +22,11 @@
         echo "Random Category: " . $out;
     }
     catch(Exception e){
-        echo e;
+        echo "error";
     }
+
+
+
+echo "</body></html>";
 
 ?>
