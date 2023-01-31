af1f4530a2b162413d4aed6820c5909422a084f9
Tim Müller <Tim.Mueller@edu.bib.de>
2023-01-31 14:41:04 +0100

    testing palyerhinzufügen

--- Scripts/Admin/playerHinzufuegen.php
+++ Scripts/Admin/playerHinzufuegen.php
@@ -21,7 +21,7 @@
 					echo "test1<br>";
 					$pcount = $conn->query("SELECT count(playerid) FROM player");
 					echo "test2<br>";
-					echo $pcount;
+					print_r($pcount);
 					echo "test3<br>";
 					$sql .= "NULL, ";
 				}
