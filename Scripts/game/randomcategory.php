e4a1aed8422b8a37b084ad31eaec4498526e9f1b
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-01 14:42:15 +0100

    testing randcat

--- Scripts/game/randomcategory.php
+++ Scripts/game/randomcategory.php
@@ -2,23 +2,27 @@
 
 /*Call this Script for API Request*/
 require 'choosecategory/categoryscript.php';
+try{
+    $out = Category::getRandomCategory();
 
-$out = Category::getRandomCategory();
+    echo 
+    '<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>randomcategory</title>
+    </head>
+    <body> 
+    Random Category: ' . $out;
+    
+    
+    
+    echo '</body>
+    </html>';
+} catch (Exception $e){
+    echo $e;
+}
 
-echo 
-'<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>randomcategory</title>
-</head>
-<body> 
-Random Category: ' . $out;
-
-
-
-echo '</body>
-</html>';
 
 ?>
