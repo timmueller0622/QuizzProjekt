7cfc2ee4a9bc8c3a9a3feeed69dc3823e9b54f4c
MarcP <marc.pape-info@web.de>
2023-02-01 14:16:23 +0100

    Test in innerScript

--- Scripts/game/randomcategory.php
+++ Scripts/game/randomcategory.php
@@ -1,10 +1,30 @@
 <?php
 
 /*Call this Script for API Request*/
-require 'choosecategory/categoryscript.php';
+//require 'choosecategory/categoryscript.php';
 
+function getRandomCategory(){
+    require '../../connectToDatabase.php';
+    
+    $GenreArray = array();
+    $pos = 0;
 
-$out = Category::getRandomCategory();
+    foreach($conn->query("SELECT * FROM Genre") as $row){
+        
+        $GenreArray[$pos] += $row;
+        $pos++;
+
+    }
+
+    $min = 0;
+    $max = 3;/*sizeof($GenreArray)*/
+    $randomNumber = rand($min, $max);
+    $categoryToReturn = $GenreArray($randomNumber);
+
+    return "Methodenaufruf erfolgreich";//$categoryToReturn;
+}
+
+$out = getRandomCategory();
 
 echo 
 '<!DOCTYPE html>
