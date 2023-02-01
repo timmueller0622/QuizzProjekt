716c1f15fdc018e75875e96c0d35b3915736af59
MarcP <marc.pape-info@web.de>
2023-02-01 13:00:46 +0100

    timwardochnichtderfehler

--- Scripts/game/choosecategory/categoryscript.php
+++ Scripts/game/choosecategory/categoryscript.php
@@ -3,8 +3,8 @@
 class Category{
 
     public static function getRandomCategory() : string{
-        //require '../../connectToDatabase.php';
-        echo "succsess connectToDatabase";
+        require '../../connectToDatabase.php';
+        /*echo "succsess connectToDatabase";
         
         $GenreArray = array();
         $pos = 0;
@@ -18,7 +18,8 @@ class Category{
 
         $categoryToReturn = $GenreArray[rand(0, sizeof($GenreArray))]['GenreDescriptor'];*/
 
-        return $categoryToReturn;
+        //return $categoryToReturn;
+        return "test";
     }
 /*
     public static function getAllCategories(){
