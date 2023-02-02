768e557fe53ebf535e060c82c4a2305033a8a709
timmueller0622 <Tim.Mueller@edu.bib.de>
2023-02-02 12:30:13 +0100

    testing pmod

--- Scripts/User/userModify.php
+++ Scripts/User/userModify.php
@@ -6,11 +6,11 @@ class ModifyUser
     public static function modifyUserPasswd($userID, $oldpasswd, $newpasswd): bool
     {
         require '../connectToDatabase.php';
-        $sql = 'SELECT * FROM Player WHERE PlayerID =' . $userID . ';';
-        $currentUser = $conn->query($sql);
+        $sql = 'SELECT * FROM Player WHERE PlayerID = '. $userID;
+        $currentUser = $conn->query($sql)->fetchAll()[0];
 
         if ($currentUser['USERPASSWORD'] == $oldpasswd) {
-            $conn->query('UPDATE TABLE Player SET UserPassword = "' . $newpasswd . '" WHERE PlayerID = ' . $userID . ';');
+            $conn->query('UPDATE TABLE Player SET UserPassword = "' . $newpasswd . '" WHERE PlayerID = ' . $userID);
             return true;
         } else {
             return false;
