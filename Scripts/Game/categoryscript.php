9c068cc427f0b628f7a1bdd320719f6a60274465
Marc Pape <Marc.Pape@edu.bib.de>
2023-02-02 12:45:52 +0100

    changed something ^^

--- Scripts/Game/categoryscript.php
+++ Scripts/Game/categoryscript.php
@@ -13,10 +13,7 @@ class Category{
         $min = 0;
         $max = count($GenreArray);
         $randomNumber = rand($min, $max);
-        //echo "random works";
-        print_r($GenreArray);
         $categoryToReturn = $GenreArray[$randomNumber];
-        print_r($categoryToReturn);
         return $categoryToReturn;
     }
     
