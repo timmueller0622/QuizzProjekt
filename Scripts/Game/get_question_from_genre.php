b029609acac73a6ff25d4fbd69bb003e8538f670
Marc Pape <Marc.Pape@edu.bib.de>
2023-02-02 15:55:11 +0100

    1

--- Scripts/Game/get_question_from_genre.php
+++ Scripts/Game/get_question_from_genre.php
@@ -2,7 +2,7 @@
 
     require 'genrescript.php';
     echo "genrescript eingebunden!";
-    $questionToEcho = Genre::getRandomQuestionFromGenre(1, 0);
+    $questionToEcho = Genre::getRandomQuestionFromGenre(1, 1);
     echo "GenreGetRandomQuestionFromGenre-call succeeded.";
     print_r($questionToEcho);
 
