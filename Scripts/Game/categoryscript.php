414e9c3e45ad6aae591a274fc7378df94c0c883f
Marc Pape <Marc.Pape@edu.bib.de>
2023-02-02 12:34:13 +0100

    nonoe

--- Scripts/Game/categoryscript.php
+++ Scripts/Game/categoryscript.php
@@ -14,7 +14,7 @@ class Category{
         $randomNumber = rand($min, $max);
         //echo "random works";
         $categoryToReturn = $GenreArray[$randomNumber];
-        return $categoryToReturn['GENREDESCRIPTOR'];
+        return $categoryToReturn;
     }
     
     static function getAllCategories()
