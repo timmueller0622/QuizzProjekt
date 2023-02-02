0ccf473a80d987f4ba4af5812b1dbf8aa8fc5066
Marc Pape <Marc.Pape@edu.bib.de>
2023-02-02 14:12:14 +0100

    test

--- Scripts/Game/get_random_category.php
+++ Scripts/Game/get_random_category.php
@@ -3,18 +3,6 @@
 /*Call this Script for API Request*/
 
     require 'categoryscript.php';
-    $out = Category::getRandomCategory();
-
-    echo 
-    '<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>randomcategory</title>
-    </head>
-    <body>' . $out;
-    
-    echo '</body></html>';
+    echo Category::getRandomCategory();
 
 ?>
