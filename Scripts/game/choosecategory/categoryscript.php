bdf37b4efe129f293395fbb748ba015b3c3c0c53
timmueller0622 <Tim.Mueller@edu.bib.de>
2023-02-02 11:54:43 +0100

    test randcat

--- Scripts/game/choosecategory/categoryscript.php
+++ Scripts/game/choosecategory/categoryscript.php
@@ -2,16 +2,20 @@
 
 function getRandomCategory()
 {
+    echo "test1";
     require '../../connectToDatabase.php';
+    echo "test2";
     $GenreArray = array();
+    echo "test3";
     foreach ($conn->query("SELECT * FROM GENRE;") as $row) {
+        echo "test4";
         $GenreArray[] .= $row;
     }
     $min = 0;
     $max = count($GenreArray);
     $randomNumber = rand($min, $max);
     $categoryToReturn = $GenreArray[$randomNumber]['GENREDESCRIPTOR'];
-
+    echo $categoryToReturn;
     return $categoryToReturn;
 }
 
