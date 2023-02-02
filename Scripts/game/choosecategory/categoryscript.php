c76a45c0fa60b90e99652fae38ee8035d65b8f9c
Marc Pape <Marc.Pape@edu.bib.de>
2023-02-02 09:54:24 +0100

    test

--- Scripts/game/choosecategory/categoryscript.php
+++ Scripts/game/choosecategory/categoryscript.php
@@ -3,28 +3,24 @@
 class Category{
 
     static function getRandomCategory(){
-        try{
-            require '../../connectToDatabase.php';
-        
+
+        require '../../connectToDatabase.php';
         $GenreArray = array();
         $pos = 0;
-
         foreach($conn->query("SELECT * FROM Genre") as $row){
             
             $GenreArray[$pos] += $row;
             $pos++;
 
         }
-
         $min = 0;
         $max = 3;/*sizeof($GenreArray)*/
         $randomNumber = rand($min, $max);
         //$categoryToReturn = $GenreArray($randomNumber);
 
         return "Methodenaufruf erfolgreich";//$categoryToReturn;
-        } catch (Exception $e){
+        
             echo $e;
-        }
         
     }
 /*
