d49aa8656f1a6499d926e31edd99fec03e9353d0
timmueller0622 <Tim.Mueller@edu.bib.de>
2023-02-02 11:09:37 +0100

    test plog

--- Scripts/Admin/loginAPI.php
+++ Scripts/Admin/loginAPI.php
@@ -53,7 +53,7 @@ session_start();
             require '../connectToDatabase.php';
             echo "test2";
             $dataToProof = $conn->query("SELECT * FROM player WHERE Username = '" . $username . "'");
-            print_r($dataToProof);
+            print_r($dataToProof->fetchAll());
             if($dataToProof['USERPASSWORD'] == $passwd){
                 return true;
             }
