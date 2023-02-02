a3a20f156478d14223034678ee50c1db56658b58
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-02 23:50:58 +0100

    testing startgame

--- Scripts/Admin/navi.php
+++ Scripts/Admin/navi.php
@@ -2,8 +2,11 @@
 if (!isset($_SESSION["username"]))
     header('Location: loginAPI.php');
 echo '<div align="center">
-    <a href="playerAnzeigen.php">Alle Player</a> |
-    <a href="playerHinzufuegen.php">Neuer Player</a> | 
-    <a href="logoutAPI.php">Logout</a>
-    </div><hr>';
+<a href="playerAnzeigen.php">Alle Player</a> |
+<a href="playerHinzufuegen.php">Neuer Player</a> | 
+<a href="logoutAPI.php">Logout</a>
+</div><hr>
+<div align="center">
+<a href="startGame.php">Spiel beginnen</a>
+</div><hr>';
 ?>
---
+++ Scripts/Admin/startGame.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+require '../connectToDatabase.php';
+if (!isset($_SESSION["username"])) {
+    header('Location: loginAPI.php');
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Genre Auswählen</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <?php
+    // http://quizzteam2.jedimasters.net/Scripts
+    require 'navi.php';
+    require '../connectToDatabase.php';
+    echo "<div align='center'>Choose Genre</div>";
+    $s = "<table align =\"center\" border= \"1\" cellpadding=\"10\" cellspacing=\"0\"><thead><tr>";
+    foreach ($conn->query('SELECT * FROM genre') as $r) {
+        for ($i = 0; $i < sizeof(array_keys($r)); $i++) {
+            if (is_numeric(array_keys($r)[$i]))
+                continue;
+            $s .= "<th>" . array_keys($r)[$i] . "</a></th>";
+        }
+        break;
+    }
+    $s .= "</tr></thead><tbody>";
+    foreach ($conn->query("SELECT * FROM genre") as $r) {
+        $s .= "<tr>";
+        for ($i = 0; $i < sizeof(array_map('htmlentities', $r)); $i++) {
+            if (is_numeric(array_keys($r)[$i]))
+                continue;
+            $s .= "<td>" . array_values($r)[$i] . "</td>";
+        }
+        $s .= "</tr>";
+    }
+    $s .= "</tbody></table>";
+    echo $s;
+    ?>
+</body>
+</html>
