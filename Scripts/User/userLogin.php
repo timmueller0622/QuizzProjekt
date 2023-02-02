3b161ebff83e43bf38e7a3ee2bdc7517a06fd795
timmueller0622 <Tim.Mueller@edu.bib.de>
2023-02-02 11:32:45 +0100

    test plog

--- Scripts/User/userLogin.php
+++ Scripts/User/userLogin.php
@@ -1,21 +1,25 @@
 <?php
 
-    class LoginUser{
-        public static function proofLoginData($passwd, $username){
+class LoginUser
+{
+    public static function proofLoginData($passwd, $username)
+    {
+        try {
             require '../connectToDatabase.php';
             $dataToProof = $conn->query("SELECT * FROM player WHERE username = '" . $username . "'")->fetchAll()[0];
-            if($dataToProof['USERPASSWORD'] == $passwd){
+            if ($dataToProof['USERPASSWORD'] == $passwd) {
                 return $dataToProof['PLAYERID'];
-            }
-            else { 
+            } else {
                 return false;
             }
-        }
-        public static function echoWrongLoginData($inputPasswd, $inputUsername) : void{
-            echo "<font color='red'> Logindaten ungültig. Es wurde versucht sich mit </font><font color='red'>" 
-                . $inputUsername . " und dem Password </font><font color='red'>" 
-                . $inputPasswd . " einzuloggen.</font>";
+        } catch (Exception $e) {
+            print_r($e);
         }
     }
-    
-?>
+    public static function echoWrongLoginData($inputPasswd, $inputUsername): void
+    {
+        echo "<font color='red'> Logindaten ungültig. Es wurde versucht sich mit </font><font color='red'>"
+            . $inputUsername . " und dem Password </font><font color='red'>"
+            . $inputPasswd . " einzuloggen.</font>";
+    }
+}
