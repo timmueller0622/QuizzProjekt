650acb344dda161a2bf603dcbb037669de159a2a
MarcP <marc.pape-info@web.de>
2023-02-03 13:33:32 +0100

    sendResetEmail improved

--- Scripts/User/userReset.php
+++ Scripts/User/userReset.php
@@ -5,7 +5,12 @@ class ResetUser
 
     public static function sendResetEmail($emailinput)
     {
-        mail($emailinput, "Quizapp reset Mail test", "ResetTestMail", "From: Tim Mueller <Tim.Mueller@edu.bib.de>");
+        $newPasswd = "testNewPasswd";
+
+        require '../connectToDatabase.php'; 
+        mail($emailinput, "Quizapp reset Mail", "Your new Password is: " . $newPasswd, "From: QuizzApp Support <Michael.Nettersheim@bib.de>");
+        $toExecute = $conn -> prepare("UPDATE TABLE PLAYER SET USERPASSWORD =" . $newPasswd . " WHERE EMAIL = " . $emailinput);
+        $toExecute->execute();
     }
 }
 ?>
