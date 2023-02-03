e2970c731184d9b529e2df4470129de8d63922e6
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-03 16:04:19 +0100

    testing ccat

--- Scripts/Admin/startGame.php
+++ Scripts/Admin/startGame.php
@@ -18,11 +18,12 @@ if (!isset($_SESSION["username"])) {
     // http://quizzteam2.jedimasters.net/Scripts
     require 'navi.php';
     require '../connectToDatabase.php';
-    $s = "<div align='center'><label for='genre'>Choose a genre: </label>";
+    $s = "<div align='center'><label for=\"genre\">Choose a genre: </label>";
     $s .= "<select name='genre' id='genre'>";
     $genres = $conn->query("SELECT genredescriptor FROM genre")->fetchAll()[0];
+    print_r($genres);
     for ($i = 0; $i < sizeof($genres); $i++) {
-        $s .= "<option value='" . $genres[$i] . "'>" .  array_values($r)[$i] . "</option>";
+        $s .= "<option value=\"" . $genres[$i] . "\">" .  array_values($r)[$i] . "</option>";
     }
     $s .= "</select></div>";
     echo $s;
