748647d908bda97a63e629b286824f69366d86c0
MarcP <marc.pape-info@web.de>
2023-02-03 12:38:35 +0100

    test round dings

--- Scripts/Game/roundscript.php
+++ Scripts/Game/roundscript.php
@@ -14,9 +14,16 @@ class Round{
     static function getAllRounds(){
         require '../connectToDatabase';
         $back = array();
-        foreach($conn->query("SELECT * FROM ROUND") as $row){
-            print_r($row);
-            $back[] .= $row['ROUNDID'] . ";" . $row['GAMEID'] . ";" . $row['SETTINGID'] . ";|";
+
+        $toExecute = $conn -> prepare("SELECT COUNT(ROUNDID) FROM ROUND");
+        $toExecute->execute();
+        $counter = ($toExecute->fetchAll(PDO::FETCH_DEFAULT))['COUNT(ROUNDID)'];
+
+        if($counter > 0){
+            foreach($conn->query("SELECT * FROM ROUND") as $row){
+                print_r($row);
+                $back[] .= $row['ROUNDID'] . ";" . $row['GAMEID'] . ";" . $row['SETTINGID'] . ";|";
+            }
         }
         print_r($back);
         return $back;
