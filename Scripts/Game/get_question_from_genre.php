f5d3cff18e38359ed11f2cb15d092295b113a95e
MarcP <marc.pape-info@web.de>
2023-02-03 09:35:48 +0100

    Does it work like it should? Think so ..

--- Scripts/Game/get_question_from_genre.php
+++ Scripts/Game/get_question_from_genre.php
@@ -2,8 +2,6 @@
 
     require 'genrescript.php';
     $output = "Aufruf fehlgeschlagen! Falsche Parameter!";
-    echo $_GET['genreid'];
-    //echo $_GET['difficultyid']
     if(isset($_GET['genreid']) && isset($_GET['difficultyid']))
     {   
         $genreID = $_GET['genreid'];
