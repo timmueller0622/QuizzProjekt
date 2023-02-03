35ad8d48101c358d3f1b17602042a2a8573fe135
MarcP <marc.pape-info@web.de>
2023-02-03 11:06:48 +0100

    das is weird

--- Scripts/Game/questionsandanswers.php
+++ Scripts/Game/questionsandanswers.php
@@ -3,9 +3,8 @@
 class QuestionData{
   
     public static function getQuestionWithAnswers($questionID){
-        echo "Methodenaufruf";
         require '../connectToDatabase.php';
-
+        echo "database";
         $toReturn = array();
 
         foreach($conn->query("SELECT QuestionData JOIN AnswerData ON QuestionData.QuestionDataID = AnswerData.Question WHERE QuestionData = " . $questionID . ";") as $entry){
