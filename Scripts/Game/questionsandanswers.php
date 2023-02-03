c34ab2911db804d3dec64e5e84673522e1cab2fa
MarcP <marc.pape-info@web.de>
2023-02-03 11:15:19 +0100

    sql statement fixed doppelt so gut wie vorher

--- Scripts/Game/questionsandanswers.php
+++ Scripts/Game/questionsandanswers.php
@@ -6,8 +6,9 @@ class QuestionData{
         require '../connectToDatabase.php';
         $toReturn = array();
         echo "vor der Schleife";
-        foreach($conn->query("SELECT * FROM QUESTIONDATA JOIN ANSWERDATA ON QUESTIONDATA.QUESTIONDATAID = ANSWERDATA.QUESTION 
-                                GROUP BY QUESTIONDATAID WHERE QUESTIONDATAID = " . $questionID . ";") as $entry){
+        foreach($conn->query("SELECT QUESTION, ANSWERID, ANSWERDESCRIPTION, ISRIGHT 
+        FROM QUESTIONDATA JOIN ANSWERDATA ON QUESTIONDATA.QUESTIONDATAID = ANSWERDATA.QUESTION 
+        WHERE QUESTIONDATAID =" . $questionID) as $entry){
             echo "entry";
             $toReturn[] .= $entry['ANSWERID'] . ";" . $entry['ANSWERDESCRIPTION'] . ";" . $entry['ISRIGHT'] . ";" . $entry['QUESTION'] . ";|" ;
 
