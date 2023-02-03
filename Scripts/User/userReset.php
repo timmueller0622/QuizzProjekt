c81d35a05d6f8e6a434fc42b0d7cf1451b65b743
MarcP <marc.pape-info@web.de>
2023-02-03 13:35:10 +0100

    randomPassword added

--- Scripts/User/userReset.php
+++ Scripts/User/userReset.php
@@ -2,10 +2,18 @@
 
 class ResetUser
 {
+    static function randomPassword() {
+        $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+        for ($i = 0; $i < 8; $i++) {
+            $n = rand(0, count($alphabet)-1);
+            $pass[$i] = $alphabet[$n];
+        }
+        return $pass;
+    }
 
-    public static function sendResetEmail($emailinput)
+    static function sendResetEmail($emailinput)
     {
-        $newPasswd = "testNewPasswd";
+        $newPasswd = randomPassword();
 
         require '../connectToDatabase.php'; 
         mail($emailinput, "Quizapp reset Mail", "Your new Password is: " . $newPasswd, "From: QuizzApp Support <Michael.Nettersheim@bib.de>");
