2576e529b1c0613d9f99d5bc1b414cf437d62342
MarcP <marc.pape-info@web.de>
2023-02-03 09:28:37 +0100

    save save lel lel

--- Scripts/Game/get_question_from_genre.php
+++ Scripts/Game/get_question_from_genre.php
@@ -2,6 +2,8 @@
 
     require 'genrescript.php';
     $output = "Aufruf fehlgeschlagen! Falsche Parameter!";
+    echo $_GET['genreid'];
+    echo $_GET['difficultyid']
     if(isset($_GET['genreid']) && isset($_GET['difficultyid']))
     {   
         $genreID = $_GET['genreid'];
