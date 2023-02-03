e91337a54a66294ef8926f0b97c82b44d424be87
MarcP <marc.pape-info@web.de>
2023-02-03 11:37:37 +0100

    fetch all test 2

--- Scripts/Game/checkanswerscript.php
+++ Scripts/Game/checkanswerscript.php
@@ -5,7 +5,8 @@ class Answer{
     public static function check_answer_is_correct($answerID){
         require '../connectToDatabase.php';
 
-        $QuestionAnswerdRight = $conn -> query("SELECT ISRIGHT FROM ANSWERDATA WHERE ANSWERID = " . $answerID);
+        $QuestionAnswerdRight = $conn -> prepare("SELECT ISRIGHT FROM ANSWERDATA WHERE ANSWERID = " . $answerID);
+        $QuestionAnswerdRight->execute();
         print_r($QuestionAnswerdRight);
         if($QuestionAnswerdRight['ISRIGHT'] == 1){
             /* true equals 1 in database*/
