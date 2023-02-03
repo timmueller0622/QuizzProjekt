e6ea4b03bff81299be9d161df2eb3f9508faa2d0
MarcP <marc.pape-info@web.de>
2023-02-03 11:10:25 +0100

    sql satatement broke?

--- Scripts/Game/questionsandanswers.php
+++ Scripts/Game/questionsandanswers.php
@@ -4,11 +4,13 @@ class QuestionData{
   
     public static function getQuestionWithAnswers($questionID){
         require '../connectToDatabase.php';
-        echo "database";
         $toReturn = array();
 
-        foreach($conn->query("SELECT QuestionData JOIN AnswerData ON QuestionData.QuestionDataID = AnswerData.Question WHERE QuestionData = " . $questionID . ";") as $entry){
+        foreach($conn->query("SELECT * FROM QUESTIONDATA JOIN ANSWERDATA ON QUESTIONDATA.QUESTIONDATAID = ANSWERDATA.QUESTION 
+                                GROUP BY QUESTIONDATAID WHERE QUESTIONDATAID = " . $questionID . ";") as $entry){
+
             $toReturn[] .= $entry['ANSWERID'] . ";" . $entry['ANSWERDESCRIPTION'] . ";" . $entry['ISRIGHT'] . ";" . $entry['QUESTION'] . ";|" ;
+
         }
 
         return $toReturn;        
