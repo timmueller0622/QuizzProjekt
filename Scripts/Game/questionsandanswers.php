fc3aafb8ec5a3ae02ae7e7e3597c58a1c2897a58
MarcP <marc.pape-info@web.de>
2023-02-03 11:11:06 +0100

    echos for test

--- Scripts/Game/questionsandanswers.php
+++ Scripts/Game/questionsandanswers.php
@@ -5,14 +5,14 @@ class QuestionData{
     public static function getQuestionWithAnswers($questionID){
         require '../connectToDatabase.php';
         $toReturn = array();
-
+        echo "vor der Schleife";
         foreach($conn->query("SELECT * FROM QUESTIONDATA JOIN ANSWERDATA ON QUESTIONDATA.QUESTIONDATAID = ANSWERDATA.QUESTION 
                                 GROUP BY QUESTIONDATAID WHERE QUESTIONDATAID = " . $questionID . ";") as $entry){
-
+            echo "entry";
             $toReturn[] .= $entry['ANSWERID'] . ";" . $entry['ANSWERDESCRIPTION'] . ";" . $entry['ISRIGHT'] . ";" . $entry['QUESTION'] . ";|" ;
 
         }
-
+        echo "nach der Schleife";
         return $toReturn;        
     }
 
