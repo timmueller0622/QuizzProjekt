a0b6d276de97430b92af7f162b8efca6b9c9faa7
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-04 23:15:01 +0100

    testing question

--- Scripts/Game/questionsandanswers.php
+++ Scripts/Game/questionsandanswers.php
@@ -3,11 +3,18 @@ class QuestionData{
     public static function getQuestionFromSettings($genreID, $difficultyID){
         require '../connectToDatabase.php';
         $toReturn = array();
+        echo "test1";
         foreach($conn->query("SELECT * FROM QUESTIONDATA
         WHERE GENRE =" . $genreID .
         "AND DIFFICULTY =" . $difficultyID) as $entry){
+            
+            echo "test2";
             $toReturn[] .= $entry['QUESTIONDATAID'] . ";" . $entry['QUESTIONDESCRIPTION'];
+            
+            echo "test3";
         }
+        
+        echo "test4";
         return $toReturn;
     }
   
