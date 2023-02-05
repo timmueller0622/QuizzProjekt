6c159f51c03a06745be501be7ca17dfbf31f5fc3
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-05 18:42:30 +0100

    test round

--- Scripts/Game/creategame.php
+++ Scripts/Game/creategame.php
@@ -1,35 +1,40 @@
 <?php
-    class Game {
-        static function createGame() {
-            require '../connectToDatabase.php';
-            $sql = "INSERT INTO game (gameid, roundcount, gametime) VALUES (?, ?, ?)";
-            $gameid = $conn->query("SELECT count(*) FROM game")->fetchAll()[0][0];
-            foreach ($conn->query("SELECT * FROM game") as $r){
-                if ($r['GAMEID'] == $gameid)
-                    $gameid++;
-            }
-            $roundcount = 2;
-            $gametime = null;
-            $stmt = $conn->prepare($sql);
-        	$stmt->execute([$gameid, $roundcount, $gametime]);
-            return $gameid;
+class Game
+{
+    static function createGame()
+    {
+        require '../connectToDatabase.php';
+        $sql = "INSERT INTO game (gameid, roundcount, gametime) VALUES (?, ?, ?)";
+        $gameid = $conn->query("SELECT count(*) FROM game")->fetchAll()[0][0];
+        foreach ($conn->query("SELECT * FROM game") as $r) {
+            if ($r['GAMEID'] == $gameid)
+                $gameid++;
         }
-
-        static function createRound($gameid, $difficulty, $genre) {
-            require '../connectToDatabase.php';
-            $sql = "INSERT INTO round (roundid, gameid, settingid) VALUES (?, ?, ?)";
-            $roundid = $conn->query("SELECT count(*) FROM round")->fetchAll()[0];
-            foreach ($conn->query("SELECT * FROM round") as $r){
-                if ($r['ROUNDID'] == $roundid)
-                    $roundid++;
-            }
-            $sql = "SELECT * FROM roundsetting WHERE genre =" . $genre . "AND difficulty =" . $difficulty;
-            $setting = $conn->query($sql)->fetchAll()[0][0];
-            $stmt = $conn->prepare($sql);
-        echo "test1";
-        	$stmt->execute([$roundid, $gameid, $setting]);
-        echo "test";
-            return $roundid;
-        }
+        $roundcount = 2;
+        $gametime = null;
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$gameid, $roundcount, $gametime]);
+        return $gameid;
     }
-?>
+
+    static function createRound($gameid, $difficulty, $genre)
+    {
+        require '../connectToDatabase.php';
+        $sql = "INSERT INTO round (roundid, gameid, settingid) VALUES (?, ?, ?)";
+        $roundid = $conn->query("SELECT count(*) FROM round")->fetchAll()[0];
+        foreach ($conn->query("SELECT * FROM round") as $r) {
+            if ($r['ROUNDID'] == $roundid)
+                $roundid++;
+        }
+        $sql = "SELECT * FROM roundsetting WHERE genre =" . $genre . "AND difficulty =" . $difficulty;
+        $setting = $conn->query($sql)->fetchAll()[0][0];
+        $stmt = $conn->prepare($sql);
+        echo "test1";
+        try {
+            $stmt->execute([$roundid, $gameid, $setting]);
+        } catch (Exception $e) {
+            echo $e;
+        }
+        return $roundid;
+    }
+}
