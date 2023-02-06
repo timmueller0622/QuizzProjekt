cf251a02be91b0f0b0f7cac2f6e004918139ce52
timmueller0622 <Tim.Mueller@edu.bib.de>
2023-02-06 10:58:02 +0100

    test qna

--- Scripts/Game/questionsandanswers.php
+++ Scripts/Game/questionsandanswers.php
@@ -2,7 +2,7 @@
 class QuestionData{
     static function getQuestionFromSettings($roundid){
         require '../connectToDatabase.php';
-        echo $roundid;
+        print_r($roundid);
         $settingid = $conn->query("SELECT genre, difficulty FROM roundsetting 
         JOIN round on round.settingid = roundsetting.settingid
         WHERE round.roundid =" . $roundid)->fetchAll()[0];
