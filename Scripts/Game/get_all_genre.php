31160ef1090f742def3a41be746e8728c5ec7b95
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-08 13:33:39 +0100

    test g all

--- Scripts/Game/get_all_genre.php
+++ Scripts/Game/get_all_genre.php
@@ -3,7 +3,7 @@
 /*Call this Script for API Request*/
 
 require 'genrescript.php';
-print_r(json_encode(Genre::getAllGenres(), JSON_FORCE_OBJECT));
+print_r(json_encode(Genre::getAllGenres()));
     
 
 ?>
