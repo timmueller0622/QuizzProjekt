9b1696804bd0842e27e3c6f009fc9eeb97261520
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-08 23:38:10 +0100

    test postlogin

--- Scripts/User/postLogin.php
+++ Scripts/User/postLogin.php
@@ -2,6 +2,6 @@
 require 'userLogin.php';
 require '../connectToDatabase.php';
 $data = json_encode(array(array('LOGINRESULT' => LoginUser::proofLoginData($_GET['password'], $_GET['username']))));
-print_r($data);
+echo ($data);
 return $data;
 ?>
