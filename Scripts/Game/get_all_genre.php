e2aef8d0001c62cec1166678d33bb6d8193f78e7
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-08 16:41:18 +0100

    test stringify

--- Scripts/Game/get_all_genre.php
+++ Scripts/Game/get_all_genre.php
@@ -14,10 +14,9 @@
         <?php
         /*Call this Script for API Request*/
         require 'genrescript.php';
+        $data = Genre::getAllGenres();
         ?>
-        JSON.stringify(
-            <?php Genre::getAllGenres();?>
-        )
+        JSON.stringify("<?php $mydata?>")
     </script>
     
 </body>
