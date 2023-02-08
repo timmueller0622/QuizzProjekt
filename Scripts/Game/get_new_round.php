347f2389f4840d6281f54246192d1c72dff22d0d
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-08 14:15:51 +0100

    test gnr

--- Scripts/Game/get_new_round.php
+++ Scripts/Game/get_new_round.php
@@ -4,6 +4,8 @@
 
 require 'creategame.php';
 echo $_GET['gameid'];
+echo $_GET['genre'];
+echo $_GET['difficulty'];
 print_r(json_encode(Game::createRound($_GET['gameid'], $_GET['genre'], $_GET['difficulty'])));
 
 ?>
