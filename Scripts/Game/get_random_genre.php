37391791535c7ec6c303976e11f5ee5e81858e5c
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-08 13:35:31 +0100

    test g random

--- Scripts/Game/get_random_genre.php
+++ Scripts/Game/get_random_genre.php
@@ -3,6 +3,6 @@
 /*Call this Script for API Request*/
 
     require 'genrescript.php';
-    print_r(json_encode(Genre::getRandomGenre(), JSON_FORCE_OBJECT)); 
+    print_r(json_encode(Genre::getRandomGenre())); 
 
 ?>
