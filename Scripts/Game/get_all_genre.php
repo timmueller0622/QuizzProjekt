dc7e4313e4f192638f55894578c5d8ab7b0e1a0d
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-08 16:56:17 +0100

    test gag

--- Scripts/Game/get_all_genre.php
+++ Scripts/Game/get_all_genre.php
@@ -2,25 +2,6 @@
 /*Call this Script for API Request*/
 require 'genrescript.php';
 $data = Genre::getAllGenres();
-print_r(gettype($data));
-$mydata = 'test';
+print_r($data);
+return $data;
 ?>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Get All Genres</title>
-    <meta charset="utf-8">
-</head>
-
-<body>
-    <!-- Form um sich einzuloggen-->
-    
-    <script>
-        alert("<?php echo Genre::getAllGenres() ?>");
-    </script>
-    
-</body>
-
-</html>
