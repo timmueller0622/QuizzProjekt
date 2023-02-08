7cd7a08761de31a24bda556e8b2f02ec67fba3fe
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-08 14:15:23 +0100

    test get new round

--- Scripts/Game/get_new_round.php
+++ Scripts/Game/get_new_round.php
@@ -3,6 +3,7 @@
 /*Call this Script for API Request*/
 
 require 'creategame.php';
+echo $_GET['gameid'];
 print_r(json_encode(Game::createRound($_GET['gameid'], $_GET['genre'], $_GET['difficulty'])));
 
 ?>
