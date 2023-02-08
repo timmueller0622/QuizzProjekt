d61f2d9785fc2b02c529201ec62118dc919a2e93
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-08 14:41:13 +0100

    test gnr

--- Scripts/Game/creategame.php
+++ Scripts/Game/creategame.php
@@ -33,6 +33,7 @@ class Game
             
         }
         echo "test5<br>";
+        print_r($conn->query("SELECT genreid FROM genre WHERE genredescriptor=" . $genre)->fetchAll());
         $genreid = $conn->query("SELECT genreid FROM genre WHERE genredescriptor=" . $genre)->fetchAll()[0][0];
         $difficultyid = $conn->query("SELECT difficultyid FROM difficulty WHERE difficultydescriptor=" . $difficulty)->fetchAll()[0][0];
         $sql2 = "SELECT * FROM roundsetting WHERE genre =" . $genreid . "AND difficulty =" . $difficultyid;
