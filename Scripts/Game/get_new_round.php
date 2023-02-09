beaa2b78c82c82354252284aff4758253a9dc991
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-09 21:11:54 +0100

    test repeat questions

--- Scripts/Game/get_new_round.php
+++ Scripts/Game/get_new_round.php
@@ -2,5 +2,5 @@
 header("Content-Type: application/json");
 require 'creategame.php';
 $data = Game::createRound($_GET['playerid'], $_GET['gameid'], $_GET['difficulty'], $_GET['genre']);
-echo "<br><br><br><br><br><br>" . json_encode($data);
+echo json_encode($data);
 ?>
