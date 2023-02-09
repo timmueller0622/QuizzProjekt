0324badffd981b73f42d6f9b0e8b456936f294f3
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-09 10:19:19 +0100

    test user register

--- Scripts/User/postRegister.php
+++ Scripts/User/postRegister.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json");
-require 'userLogin.php';
+require 'userRegister.php';
 $data = json_encode(array('REGISTER' => array('REGISTERRESULT' => RegisterUser::createNewUser($_GET['username'], $_GET['password'], $_GET['email']))));
 echo $data;
 ?>
