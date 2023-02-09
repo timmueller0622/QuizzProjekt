6615ef1085f6e8c08a4b1eecb1fc6b3758cb6b58
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-09 15:16:00 +0100

    test stats

--- Scripts/Game/statistics.php
+++ Scripts/Game/statistics.php
@@ -4,12 +4,9 @@ class Statistics{
 
     static function getMatchHistory($playerid){
         require '../connectToDatabase.php';
-        echo 'test1<br>';
         $toReturn = array();
         $matchH = $conn->query("SELECT * FROM matchhistory WHERE playerid = ". $playerid)->fetchAll();
-        echo 'test2<br>';
         for ($i = 0; $i < sizeof($matchH); $i++){
-            echo 'test3<br>';
             $toReturn['MATCH' . $i] = array('PLAYERID' => $matchH[$i]['PLAYERID'], 'WON' => $matchH[$i]['WON'], 'LOST' => $matchH[$i]['LOST'], 'DRAW' => $matchH[$i]['DRAW'],
                 'GAME' => array(Statistics::getGame($matchH[$i]['GAMEID']))
             );
@@ -24,7 +21,6 @@ class Statistics{
         $toReturn['GAMEID'] = $game['GAMEID'];
         $toReturn['ROUNDCOUNT'] = $game['GAMEID'];
         $toReturn['GAMETIME'] = $game['GAMEID'];
-        echo 'testgame<br>';
         $toReturn['ROUND'] = Statistics::getRounds($gameid);
         return $toReturn;
     }
@@ -32,7 +28,6 @@ class Statistics{
     static function getRounds($gameid){
         require '../connectToDatabase.php';
         $toReturn = array();
-        echo 'testround<br>';
         $rounds = $conn->query("SELECT * FROM round WHERE gameid=". $gameid)->fetchAll();
         foreach($rounds as $round){
             $toReturn['ROUNDID'] = $round['ROUNDID'];
@@ -44,13 +39,9 @@ class Statistics{
 
     static function getSettings($settingid){
         require '../connectToDatabase.php';
-        echo 'settingid: ' . $settingid;
         $setting = $conn->query("SELECT * FROM roundsetting WHERE settingid=". $settingid)->fetchAll()[0];
-        print_r($setting);
         $genre = $conn->query("SELECT genredescriptor FROM genre WHERE genreid=". $setting['GENRE'])->fetchAll()[0][0];
-        echo 'test2<br>';
         $difficulty = $conn->query("SELECT difficultydescriptor FROM difficulty WHERE difficultyid=". $setting['DIFFICULTY'])->fetchAll()[0][0];
-        echo 'test3<br>';
         $toReturn = array('SETTINGID' => $setting['SETTINGID'], 'QUESTIONSPERROUND' => $setting['QUESTIONSPERROUND'], 'GENRE' => $genre, 'DIFFICULTY' => $difficulty);
         return $toReturn;
     }
@@ -58,9 +49,7 @@ class Statistics{
     static function getQuestions($roundid){
         require '../connectToDatabase.php';
         $toReturn = array();
-        echo 'test1<br>';
         $questions = $conn->query("SELECT * FROM question WHERE roundid=". $roundid)->fetchAll();
-        echo 'test2<br>';
         foreach($questions as $question){
             $toReturn['QUESTIONID'] = $question['QUESTIONID'];
             $toReturn['QUESTIONDATAID'] = $question['QUESTIONDATAID'];
