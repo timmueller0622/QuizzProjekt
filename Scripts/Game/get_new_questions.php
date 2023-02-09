527337e6ebf484db8121d5e5a854c381ab2bc1cb
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-09 10:58:12 +0100

    test getquestio

--- Scripts/Game/get_new_questions.php
+++ Scripts/Game/get_new_questions.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json");
-require 'creategame.php';
-$data = json_encode(Game::createQuestions($_GET['qpr'], $_GET['roundid']));
+require 'questionsandanswers.php';
+$data = json_encode(QuestionData::getQuestion($_GET['qpr']));
 echo $data;
 ?>
