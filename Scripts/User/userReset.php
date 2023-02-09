090bd0e4dc5c383474b496daf610cf319dc4913f
Tim Müller <Tim.Mueller@edu.bib.de>
2023-02-09 19:13:17 +0100

    test email

--- Scripts/User/userReset.php
+++ Scripts/User/userReset.php
@@ -36,7 +36,7 @@ $header = "From:Tim.Mueller@edu.bib.de \r\n";
 $header .= "MIME-Version: 1.0\r\n";
 $header .= "Content-type: text/html\r\n";
 
-$retval = mail($to, $subject, $message, $header);
+$retval = mail($to, $subject, $message, '');
 if ($retval == true) {
     echo "Message sent successfully...";
 } else {
