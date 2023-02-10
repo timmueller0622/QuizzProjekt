171d6c3400f3c2ff540a062e733e0fd56afa3a5d
timmueller0622 <Tim.Mueller@edu.bib.de>
2023-02-10 09:49:54 +0100

    quick removal link to deprecated function

--- Scripts/Admin/navi.php
+++ Scripts/Admin/navi.php
@@ -5,8 +5,5 @@ echo '<div align="center">
 <a href="playerAnzeigen.php">Alle Player</a> |
 <a href="playerHinzufuegen.php">Neuer Player</a> | 
 <a href="logoutAPI.php">Logout</a>
-</div><hr>
-<div align="center">
-<a href="startGame.php">Spiel beginnen</a>
 </div><hr>';
 ?>
